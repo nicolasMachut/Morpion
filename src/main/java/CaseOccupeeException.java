3297ebc29c805ff248b0b46b8db245752a9ef717
nicolasMachut <machut.nicolas@gmail.com>
2015-02-17 16:48:21 +0100

    fin de jeu ok

---
+++ src/main/java/CaseOccupeeException.java
@@ -0,0 +1,17 @@
+/**
+ * Created by nicolas on 17/02/15.
+ */
+public class CaseOccupeeException extends Exception {
+
+    public CaseOccupeeException () {
+        super();
+    }
+
+    public CaseOccupeeException(String message) {
+        super(message);
+    }
+
+
+
+
+}
---
+++ src/test/java/ControleRegleTest.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by nicolas on 17/02/15.
+ */
+public class ControleRegleTest {
+
+    private Plateau plateau;
+
+    @Before
+    public void createPlateauTest(){
+        this.plateau = new Plateau(9);
+    }
+
+    @Test
+    public void testUneLigneGagnante() throws Exception {
+        plateau.poserPion(new Case(1, 0, new Pion("X")));
+        plateau.poserPion(new Case(1, 1, new Pion("X")));
+        Case caseChoisie = new Case(1, 2, new Pion("X"));
+        plateau.poserPion(caseChoisie);
+        assertEquals("X", new ControleRegle(plateau, 3).verifierLigneGagnante(caseChoisie));
+    }
+
+    @Test
+    public void testUneColonneGagnante() throws Exception{
+        plateau.poserPion(new Case(0, 1, new Pion("X")));
+        plateau.poserPion(new Case(1, 1, new Pion("X")));
+        Case caseChoisie = new Case(2, 1, new Pion("X"));
+        plateau.poserPion(caseChoisie);
+        assertEquals("X", new ControleRegle(plateau, 3).verifierColonneGagnante(caseChoisie));
+    }
+
+    @Test
+    public void testDiagonaleGaucheGagnante () throws Exception {
+        plateau.poserPion(new Case(0, 0, new Pion("X")));
+        plateau.poserPion(new Case(1, 1, new Pion("X")));
+        Case caseChoisie = new Case(2, 2, new Pion("X"));
+        plateau.poserPion(caseChoisie);
+        assertEquals("X", new ControleRegle(plateau, 3).verifierDiagonaleGagnante(caseChoisie));
+    }
+
+    @Test
+    public void testDiagonaleDroiteGagnante () throws Exception {
+        plateau.poserPion(new Case(0, 2, new Pion("X")));
+        plateau.poserPion(new Case(1, 1, new Pion("X")));
+        Case caseChoisie = new Case(2, 0, new Pion("X"));
+        plateau.poserPion(caseChoisie);
+        assertEquals("X", new ControleRegle(plateau, 3).verifierDiagonaleGagnante(caseChoisie));
+    }
+
+
+
+}
